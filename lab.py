cfe845aa4a48c58a9595f52621f08aba1d73f569
ejr2180 <ejr2180@columbia.edu>
2025-04-03 10:05:22 -0400

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,1 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+@author: [NAME & UNI]
+'''
 
+'''
+Before you follow the tutorial do the following:
+1. Make sure you have the geopandas library installed (If you aren't sure if you have it, type "pip install geopandas" into the terminal
+2. Download the December 2022 Yellow Taxi Trips Records from "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page" and put them into the folder named "data"
+'''
