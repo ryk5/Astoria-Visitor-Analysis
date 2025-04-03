358330b8b7f5dd6074bc498f02ff1d9deff1a493
ejr2180 <ejr2180@columbia.edu>
2025-04-03 10:37:06 -0400

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,10 +1,67 @@
 # -*- coding: utf-8 -*-
 '''
 @author: [NAME & UNI]
-'''
 
-'''
 Before you follow the tutorial do the following:
 1. Make sure you have the geopandas library installed (If you aren't sure if you have it, type "pip install geopandas" into the terminal
 2. Download the December 2022 Yellow Taxi Trips Records from "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page" and put them into the folder named "data"
 '''
+
+#Step 1: Import necessary libraries
+'''
+INSERT CODE HERE
+'''
+
+#Step 2: Import Census Block map and data
+'''
+INSERT CODE HERE
+'''
+
+#Step 3: Import Taxi Zone map and data
+'''
+INSERT CODE HERE
+'''
+
+#Step 3: Define the Hudson Yards boundaries
+'''
+INSERT CODE HERE
+'''
+
+#Step 4: Map what we've done so far
+'''
+INSERT CODE HERE
+'''
+
+#Step 5: Import mobile phone Data
+'''
+INSERT CODE HERE
+'''
+
+#Step 5: Isolate the mobile phone data with trips to Hudson Yards
+'''
+INSERT CODE HERE
+'''
+
+#Step 6: Merge the origin of the mobile phone data with their corresponding Taxi Zones
+'''
+INSERT CODE HERE
+'''
+
+#Step 7: Map the results so far
+'''
+INSERT CODE HERE
+'''
+
+#Step 8: Import the Yellow Taxi data
+'''
+INSERT CODE HERE
+'''
+
+#Step 9: 
+'''
+INSERT CODE HERE
+'''
+
+
+
+
