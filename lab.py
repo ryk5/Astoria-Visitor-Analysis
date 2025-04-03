7ca4230bb2d8044f96a8ad03ac67004458df83d8
dcurtinmines <134435812+dcurtinmines@users.noreply.github.com>
2025-04-03 14:42:01 -0400

    changed step numbers

--- lab.py
+++ lab.py
@@ -23,42 +23,42 @@ INSERT CODE HERE
 INSERT CODE HERE
 '''
 
-#Step 3: Define the Hudson Yards boundaries
+#Step 4: Define the Hudson Yards boundaries
 '''
 INSERT CODE HERE
 '''
 
-#Step 4: Map what we've done so far
+#Step 5: Map what we've done so far
 '''
 INSERT CODE HERE
 '''
 
-#Step 5: Import mobile phone Data
+#Step 6: Import mobile phone Data
 '''
 INSERT CODE HERE
 '''
 
-#Step 5: Isolate the mobile phone data with trips to Hudson Yards
+#Step 7: Isolate the mobile phone data with trips to Hudson Yards
 '''
 INSERT CODE HERE
 '''
 
-#Step 6: Merge the origin of the mobile phone data with their corresponding Taxi Zones
+#Step 8: Merge the origin of the mobile phone data with their corresponding Taxi Zones
 '''
 INSERT CODE HERE
 '''
 
-#Step 7: Map the results so far
+#Step 9: Map the results so far
 '''
 INSERT CODE HERE
 '''
 
-#Step 8: Import the Yellow Taxi data
+#Step 10: Import the Yellow Taxi data
 '''
 INSERT CODE HERE
 '''
 
-#Step 9: 
+#Step 11: 
 '''
 INSERT CODE HERE
 '''
